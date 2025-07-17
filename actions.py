f5a1c78a522305e63510e539293af8db1819659f
ThiagoPassos212223 <thiagopassos010906@outlook.com>
2025-07-16 22:36:27 -0300

    atualização do funcionamento do sistema
    Organização do programa foi alterado para facilitar a manutenção e adição de novas funcionalidades. Agora as partes dos programas estão em arquivos separados, uma prática conhecido como modulação. Ela permite que o sistema seja mais flexivel e que seja mais fácil encontrar possíveis erros e bugs. Além disso, correção de nomenclatura inadequada das variáveis e exclusão de repetições de instruções.

---
+++ actions.py
@@ -0,0 +1,255 @@
+from utils import limparTela, exibirLinha, sortearNumero
+
+# Molda outras classes, facilitando a utilização de funções em comum. 
+class Acao:
+    def __init__(self, nome, fonte, consumo, precisao, alvo_usuario=False):
+        self.nome = nome
+        self.tipo = "acao"
+        self.fonte = fonte
+        self.consumo = consumo
+        self.precisao = precisao
+        self.alvo_usuario = alvo_usuario
+        self.funcoes = None
+    
+    def adicionarFuncoes(self, funcoes):
+        self.funcoes = funcoes
+
+    def definirUsuario(self, usuario):
+        self.usuario = usuario
+
+    def definirAlvo(self, lista_alvos):
+        # limpando a tela
+        limparTela(esperar=False)
+        
+        alvos = lista_alvos.copy()
+        # remove o usuário da lista de alvos possíveis
+        if self.alvo_usuario:
+            self.alvo = self.usuario
+            return None
+        else:
+            alvos.remove(self.usuario)
+            if len(alvos) == 1:
+                self.alvo = alvos[0]
+                return None
+            else:
+                for indice, alvo in enumerate(alvos):
+                    print(f"{indice}){alvo.nome}")
+                try:
+                    indice_alvo = int(input("selecione o alvo da sua ação: "))
+                    self.alvo = alvos[indice_alvo]
+                    return None     
+                except ValueError:
+                    print(f"ERRO: o tipo esperado é um valor númerico inteiro positivo! Tente novamente!")
+                except IndexError:
+                    print("ERRO: opção inválida! Selecione uma das opções disponíveis!")
+
+        # repete a função caso ocorra algum erro. A função só encerra o looping caso o usuário tenha selecionado um alvo válido.
+        return self.definirAlvo(lista_alvos)
+    
+    def exibirInformacoes(self):
+        dicionario_atributos = self.__dict__.items()
+        for chave, valor in dicionario_atributos:
+            if chave == "alvo_usuario" or valor == None or valor == 0:
+                pass
+            elif chave == "funcoes":
+                pass
+            else: 
+                print(f"{chave}: {valor}")
+
+    def podeSerUtilizado(self):
+        if self.fonte == "energia":
+            # reduz a energia do usuário do feitiço
+            if self.usuario.energia - self.consumo > 0:
+                return True
+            else:
+                return False
+        elif self.fonte == "pp":
+            if self.consumo - 1 >= 0:
+                return True
+            else:
+                return False
+        else:
+            print(f"ocorreu um erro! Fonte inválida para o movimento {self.nome}")
+            input("pressione enter para continuar: ")
+
+    def consumir(self):
+        if self.fonte == "energia":
+            self.usuario.energia -= self.consumo
+        elif self.fonte == "pp":
+            self.consumo -= 1
+        else:
+            print(f"ocorreu um erro! Fonte inválida para o movimento {self.nome}")
+        
+    def utilizar(self):
+        if self.usuario.analisarStatus() and self.usuario.vida > 0:
+            self.consumir()
+            print(f"{self.usuario.nome} utilizou {self.nome}")
+            if (self.usuario.precisao + self.precisao) / 2 >= sortearNumero(1, 100):
+                print(f"atingiu {self.alvo.nome}")
+                for funcao in self.funcoes:
+                    funcao.executar(self.alvo)
+            else:
+                print("Não conseguiu acertar o movimento!")
+
+        exibirLinha()
+        self.alvo = None
+        self.usuario = None
+    
+class Dano:
+    def __init__(self, dano, precisao=100, acerto_garantido=False):
+        self.dano = dano
+        self.precisao = precisao
+        self.acerto_garantido = acerto_garantido
+
+    def executar(self, alvo):
+        if self.acerto_garantido or self.precisao >= sortearNumero(1, 100):
+            print(f"causou {self.dano} de dano")
+            alvo.vida -= self.dano
+        else:
+            print("não conseguiu atingir o alvo.")
+
+class Efeito:
+    def __init__(self, status, duracao, precisao=100, acerto_garantido=False):
+        self.status = status
+        self.duracao = duracao
+        self.precisao = precisao
+        self.acerto_garantido = acerto_garantido
+    
+    def executar(self, alvo):
+        if self.acerto_garantido or self.precisao >= sortearNumero(1, 100):
+            print(f"alterou o status de {alvo.nome} para {self.status}")
+
+            if len(self.duracao) > 1:
+                duracao_efeito = sortearNumero(self.duracao[0], self.duracao[1])
+            else:
+                duracao_efeito = self.duracao
+
+            if self.status not in alvo.status:
+                alvo.status.append([self.status, duracao_efeito])
+        
+class Expansao:
+    def __init__(self, nome, consumo):
+        self.tipo = "expansao"
+        self.nome = nome
+        self.consumo = consumo
+        self.ativada = False
+    
+    def definirUsuario(self, usuario):
+        self.usuario = usuario
+
+    def definirAlvos(self, personagens):
+        self.alvos = personagens.copy()
+        self.alvos.remove(self.usuario)
+
+    def podeSerUtilizado(self):
+        if self.usuario.energia - self.consumo >= 0:
+            return True
+        else: 
+            return False
+    
+    def utilizar(self):
+        self.usuario.energia -= self.consumo
+
+    def ativar(self):
+        print(f"{self.usuario.nome} está invocando sua expansão de domínio")
+        if self.podeSerUtilizado():
+            print(f"{self.nome} está ativa")
+            self.ativada = True
+        else:
+            print(f"{self.nome} não é possível ser ativada. Energia insuficiente.")
+            
+        return self.ativada
+    
+    def executar(self):
+        self.utilizar()
+        for alvo in self.alvos:
+            match self.nome:
+                case "santuario":
+                    dano = 25
+                    alvo.vida -= dano
+                    print(f"{alvo.nome} foi atingido pelos cortes do santuario! recebeu {dano} de dano!")
+                case "muriokusho":
+                    if "imobilizado" not in alvo.status:
+                        alvo.status.append(["imobilizado", 1])
+                        print(f"{alvo.nome} foi atingido pelos efeitos do Muriokusho! Está imobilizado!")
+                
+                case _:
+                    print("essa expansão ainda não foi cadastrada, consulte o desenvolvedor para mais informações!")
+
+     
+    def exibirInformacoes(self):
+        dicionario_atributos = self.__dict__.items()
+        for chave, valor in dicionario_atributos:
+            if chave == "usuario" or valor == None or valor == 0:
+                pass
+            elif chave == "ativada":
+                if valor: 
+                    print("expansão está ativada")
+                else:
+                    print("expansão está desativada")
+            else: 
+                print(f"{chave}: {valor}")
+
+class ConjuntoAcoes:
+    def __init__(self):
+        ...
+
+    def adicionarFeiticos(self, nome_personagem):
+        # gojo
+        mugen = Acao(nome="mugen", precisao=100, fonte="energia", consumo=20)
+        mugen.adicionarFuncoes([Efeito(status="mugen ativado", duracao=[3, 4], precisao=100, acerto_garantido=True)])
+
+        azul = Acao(nome="azul", precisao=100, fonte="energia", consumo=15)
+        azul.adicionarFuncoes([Efeito(status="imobilizado", duracao=[1, 3], precisao=100, acerto_garantido=True)])
+        
+        vermelho = Acao(nome="vermelho", precisao=100, fonte="energia", consumo=35)
+        vermelho.adicionarFuncoes([Dano(dano=35, precisao=100, acerto_garantido=True), Efeito("atordoado", [1, 2], precisao=30, acerto_garantido=False)])
+        
+        vazio_roxo = Acao(nome="vazio roxo", precisao=100, fonte="energia", consumo=60)
+        vazio_roxo.adicionarFuncoes([Dano(70, precisao=100, acerto_garantido=True)])
+        # sukuna
+        clivar = Acao(nome="clivar", precisao=100, fonte="energia", consumo=25)
+        clivar.adicionarFuncoes([Dano(15, precisao=100, acerto_garantido=True), Efeito("sangrando", [1, 3], precisao=45, acerto_garantido=False)])
+
+        desmantelar = Acao(nome="desmantelar", precisao=100, fonte="energia", consumo=80)
+        desmantelar.adicionarFuncoes([Dano(50, precisao=100, acerto_garantido=True), Efeito("sangrando", [1, 3], precisao=100, acerto_garantido=False)])
+
+        flecha_fogo = Acao(nome="flecha de fogo", precisao=100, fonte="energia", consumo=65)
+        flecha_fogo.adicionarFuncoes([Dano(65, precisao=100, acerto_garantido=True), Efeito("queimando", [2, 4], precisao=55, acerto_garantido=False)])
+
+        self.feiticos = {
+            "Satoru Gojo": [mugen, azul, vermelho, vazio_roxo],
+            "Ryomen Sukuna": [clivar, desmantelar, flecha_fogo] 
+        }
+        return self.feiticos[nome_personagem]
+
+    def adicionarMovimentos(self, nome_personagem):
+        # golpes gerais
+        soco = Acao(nome="soco simples", precisao=100, fonte="pp", consumo=25)
+        soco.adicionarFuncoes([Dano(8, precisao=100, acerto_garantido=True)])
+        chute = Acao(nome="Chute tranversal", precisao=100, fonte="pp", consumo=15)
+        chute.adicionarFuncoes([Dano(12, precisao=100, acerto_garantido=True), Efeito("atordoado", [1, 3], precisao=100, acerto_garantido=False)])
+        
+        self.golpes = {
+            "Satoru Gojo": [soco, chute],
+            "Ryomen Sukuna": [soco, chute]
+        }
+        return self.golpes[nome_personagem]
+
+    def adicionarExpansoes(self, nome_personagem):
+        # expansão
+        muriokusho = Expansao(nome="muriokusho", consumo=35)
+        santuario = Expansao(nome="santuario", consumo=40)
+
+        self.expansoes = {
+            "Satoru Gojo": muriokusho,
+            "Ryomen Sukuna": santuario
+        }
+        return self.expansoes[nome_personagem]
+
+    def adicionarAcoesPersonagem(self, personagem):
+        feiticos = self.adicionarFeiticos(personagem.nome)
+        movimentos = self.adicionarMovimentos(personagem.nome)
+        expansao = self.adicionarExpansoes(personagem.nome)
+
+        personagem.adicionarAcoes(feiticos, movimentos, expansao)
---
+++ character.py
@@ -0,0 +1,125 @@
+from utils import limparTela, exibirLinha, selecioneOpcao
+
+# classe responsável por modelar os feiticeiros e maldições
+class Personagem:
+    """classe responsável por modelar os personagens, definindo seus atributos e comportamentos como analisarStatus, escolherAcao. """
+    def __init__(self, nome, vida, agilidade, energia, energia_reversa=False):
+        # atributos básicos
+        self.nome = nome
+        self.vida = vida
+        self.agilidade = agilidade
+        self.evasao = 10
+        self.energia = energia
+        self.energia_reversa = energia_reversa
+        # atributos padrão (todos os personagens possuem o mesmo valor)
+        self.precisao = 100
+        self.status = []
+
+        self.expansao_ativada = False
+    
+    def adicionarAcoes(self, feiticos, movimentos, expansao):
+        self.feiticos = feiticos
+        self.movimentos = movimentos
+        self.expansao = expansao
+
+    def analisarStatus(self):
+        joga = True
+        print(f"status: ({self.nome})")
+        # self.status é uma lista que possuí todos os status que o jogador possuí, os status são analisados nesse looping 
+        for status in self.status:
+            # efeito_status: recebe um string contendo o efeito de status
+            # duração: recebe um valor inteiro contendo a duração em turnos do efeito
+            efeito_status, duracao = status[0], status[1]
+            match efeito_status:
+                case "queimando":
+                    print(f"{self.nome} está queimando! recebeu {5} de dano.")
+                    self.vida -= 5
+                case "sangrando":
+                    print(f"{self.nome} está sangrando! recebeu {self.vida * 0.1} de dano.")
+                    self.vida -= self.vida * 0.1
+                case "imobilizado":
+                    print(f"{self.nome} está imobilizado! Não é possível realizar sua ação.") 
+                    joga = False
+                case "atordoado":
+                    print(f"{self.nome} está atordoado! Não poderá realizar sua ação.")
+                    joga = False
+                case _:
+                    print(f"ERRO! Um status não cadastrado foi detectado! {efeito_status}")
+            # verifica se a duração do efeito de status acabou, caso tenha, remove da lista de efeito de status
+            if duracao - 1 == 0:
+                self.status.remove(status)
+            # reduz a duração do efeito de status
+            else: 
+                status[1] -= 1
+        exibirLinha()
+        return joga
+
+    def definirModo(self, modo="ia"):
+        """define se o personagem sera controlado por uma IA ou um jogador"""
+        self.modo = "ia" if modo == "ia" else "jogador"
+
+    def escolherAcao(self, alvos):
+        """responsável por permitir a escolha das ações do personagem"""
+        limparTela()
+        if self.modo == "ia":
+            ...
+        else:
+            while True:
+                print(f"seleção de movimentos: ({self.nome})")
+                print(f"vida: {self.vida}   energia: {self.energia}")
+
+                dicionario_opcoes = [self.movimentos, self.feiticos]            
+                # caso o personagem possua expansão, ela será exibida, caso contrário, não
+                if self.expansao != None:
+                    dicionario_opcoes.append(self.expansao)
+
+                # verifica a seleção do usuário e com isso, carrega a lista de opções correta. 
+                acoes = selecioneOpcao(lista_exibicao=["movimentos", "feitiços", "expansao"], lista_original=dicionario_opcoes, mensagem="selecione sua ação: ")
+
+                if type(acoes) == list:
+                    if len(acoes) > 1:
+                        lista_amostra = []
+                        for acao in acoes:
+                            lista_amostra.append(acao.nome) 
+                        acao_escolhida = selecioneOpcao(lista_exibicao=lista_amostra, lista_original=acoes, mensagem="selecione um movimento: ", escolha_obrigatoria=False) 
+                        if acao_escolhida == None:
+                            return self.escolherAcao(alvos)
+                    else:
+                        acao_escolhida = acoes[0]
+
+                else:
+                    acao_escolhida = acoes
+
+                # exibe informações sobre o movimento, feitiço ou expansão escolhida
+                exibirLinha()
+                print("informações adicionais")
+                acao_escolhida.exibirInformacoes()
+
+                escolha = selecioneOpcao(lista_exibicao=["confirmar escolha", "cancelar"], lista_original=[True, False], mensagem="selecione uma das opções: ", escolha_obrigatoria=True)
+                if escolha:
+                    if acao_escolhida.tipo == "acao" and not(acao_escolhida.alvo_usuario):
+                        acao_escolhida.definirUsuario(self)
+                        acao_escolhida.definirAlvo(alvos)
+                    else:
+                        acao_escolhida.definirUsuario(self)
+
+                    pode_utilizar = acao_escolhida.podeSerUtilizado()
+
+                    if pode_utilizar:
+                        return acao_escolhida
+                    else:
+                        return self.escolherAcao(alvos)
+                else:
+                    return self.escolherAcao(alvos)
+
+class Personagens:
+    def __init__(self):
+        ...
+        
+    def adicionarPersonagens(self):
+        personagens = []
+        personagens.append(Personagem(nome="Satoru Gojo", vida=100, agilidade=100, energia=100, energia_reversa=True))
+        personagens.append(Personagem(nome="Ryomen Sukuna", vida=100, agilidade=90, energia=100, energia_reversa=True))
+
+        return personagens
+    
