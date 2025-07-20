e7cc523d7adc4b893bdb17184fd08e6e00939155
ThiagoPassos212223 <thiagopassos010906@outlook.com>
2025-07-20 11:42:09 -0300

    adicionando Yuta

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-from utils import limparTela, selecioneOpcao, sortearNumero
-from character import Personagens
-from actions import ConjuntoAcoes
-from battle import Batalha
+from utilidades import limparTela, selecioneOpcao, sortearNumero
+from personagem import Personagens
+from acoes import ConjuntoAcoes
+from batalha import Batalha
 
 # classe principal onde o jogo se passa.
 class Main:
     def __init__(self):
+        self.numero_jogadores = 2
+        self.jogadores = []
         # looping principal, menu inicial.
         print("bem vindo!")
-        print("modos de jogo: ")
-        lista_modos_jogo = ["jogador vs jogador", "jogador vs computador", "sair"]
-
-        modo_jogo =  selecioneOpcao(lista_exibicao=lista_modos_jogo, lista_original=["a", "B", "c"], mensagem="selecione o modo de jogo: ")
+        
+        lista_modos_jogo = ["jogador vs jogador", "sair"]
+        modo_jogo =  selecioneOpcao(lista_exibicao=lista_modos_jogo, lista_original=["a", "b"], mensagem="selecione o modo de jogo: ")
 
         if "a" in modo_jogo:
             self.jogadorVsJogador() # redireciona o jogador para o modo_jogo jogador VS jogador
-        elif "b" in modo_jogo:
-            self.jogadorVsComputador() # redireciona o jogador para o modo_jogo jogador VS computador
         else:
             exit() # fecha o programa
 
-    def escolherPersonagem(self, modo="automatico"):
+    def escolherPersonagem(self, modo):
         """É responsável por permitir que o usuário escolha os movimentos e feitiços"""
         # personagens disponíveis
         personagens = Personagens().adicionarPersonagens()
@@ -28,34 +27,29 @@ class Main:
             ConjuntoAcoes().adicionarAcoesPersonagem(personagem)
     
         limparTela()
-        if modo == "automatico":
+        if modo == "aleatoria":
             # sorteando um número aleatório para escolher o indíce da lista personagens. 
             indice_sorteado = sortearNumero(0, len(personagens) - 1)
             # escolhe o personagem no indice da lista personagens e armazena na variavel personagem 
             personagem = personagens[indice_sorteado]
-            return personagem 
-        else:
+
+        elif modo == "manual":
             lista_personagens = []
             # looping responsável por exibir todos os personagens
-            for n, personagem in enumerate(personagens):
+            for personagem in personagens:
                 lista_personagens.append(personagem.nome)
-
             personagem = selecioneOpcao(lista_exibicao=lista_personagens, lista_original=personagens, mensagem="selecione um personagem: ")
-            personagem.definirModo("manual")
-            return personagem
             
+        return personagem
+                  
     def jogadorVsJogador(self):
-        jogador1 = self.escolherPersonagem("manual")
-        jogador2 = self.escolherPersonagem("manual")
+        for n in range(1, self.numero_jogadores + 1):
+            print(f"player {n} vai escolher o personagem: ")
+            opcoes = ["manual", "aleatoria"]
+            modo = selecioneOpcao(lista_exibicao=opcoes, lista_original=opcoes, mensagem="selecione o modo de escolha: ", escolha_obrigatoria=True)
+            self.jogadores.append(self.escolherPersonagem(modo))
 
-        batalha = Batalha([jogador1, jogador2])
+        batalha = Batalha(self.jogadores)
         batalha.looping()
-
-    def jogadorVsComputador(self):
-        jogador1 = self.escolherPersonagem("manual")
-        jogador2 = self.escolherPersonagem("automatico")
-
-        batalha = Batalha([jogador1, jogador2])
-        batalha.looping()
-
+        
 Main()
